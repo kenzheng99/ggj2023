c18939dcb3d0661ad9ad48bfed6274f735c2389d
Brandan Chen <bychen@andrew.cmu.edu>
2023-02-04 18:46:24 -0500

    farm map

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,21 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-    [SerializeField] private float moveSpeed;
-    // Start is called before the first frame update
-    
-    void Start() {
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] public float playerSpeed = 2;
+    private Rigidbody2D rb;
+    private Animator anim;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update() {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
+    void Update()
+    {
+        rb.velocity = Vector2.zero;
 
-        Vector3 movementVector = new Vector3(inputX, inputY, 0) * moveSpeed * Time.deltaTime;
-        transform.Translate(movementVector);
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity += Vector2.up * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity += Vector2.down * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity += Vector2.right * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity += Vector2.left * playerSpeed;
+        }
 
+        rb.velocity = rb.velocity.normalized * playerSpeed;
+        anim.SetFloat("moveX", rb.velocity.x);
+        anim.SetFloat("moveY", rb.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            anim.SetFloat(("lastMoveX"), Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat(("lastMoveY"), Input.GetAxisRaw("Vertical"));
+        }
     }
 }
