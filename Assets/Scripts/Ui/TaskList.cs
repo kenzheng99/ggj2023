893b5926a300abc72f4ab79e8327a36936aa253d
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2023-02-05 11:43:43 -0500

    completed functionality for task list

--- Assets/Scripts/Ui/TaskList.cs
+++ Assets/Scripts/Ui/TaskList.cs
@@ -2,19 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TaskList : MonoBehaviour
 {
-    public Text tasks;
+    public TMP_Text tasksTMP;
+    private Dictionary<PlantType, int> taskDict;
+    private GameManager gm;
 
-    public void CreateTaskList()
-    {
-        tasks.text = "";
-        for (var i = 0; i < 4; i++)
+    private void Start() {
+        gm = GameManager.Instance;
+    }
+    
+    public void GenerateTasks() {
+        taskDict = new Dictionary<PlantType, int>();
+        taskDict[PlantType.PLANT1] = Random.Range(2, 10);
+        taskDict[PlantType.PLANT2] = Random.Range(2, 10);
+        taskDict[PlantType.PLANT3] = Random.Range(2, 10);
+    }
+    
+    public void FillTaskList() { 
+        Debug.Log("Filling Task List");
+        tasksTMP.text = "";
+        foreach(KeyValuePair<PlantType, int> plant in taskDict)
         {
-            var task = "Harvest " + (Random.Range(1,10)).ToString() 
-                                  + " type" + (Random.Range(1,3)).ToString();
-            tasks.text += task + "\n";
+            var task = "Harvest " + plant.Value.ToString() 
+                                  + " " + DataUtils.PlantToString(plant.Key)+"s";
+
+            if (plant.Value == gm.numHarvested[plant.Key]) {
+                task = "<s>" + task + "</s>";
+            }
+            Debug.Log(DataUtils.PlantToString(plant.Key) + " "+ gm.numHarvested[plant.Key]);
+            tasksTMP.text += task + "\n";        
         }
     }
 }
